7302394a2fad93d33afcb00e16b2a81b910a97c9
uTatsii <utatsiy@gmail.com>
2022-08-22 20:44:48 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', newName);
+
+function newName(event) {
+  output.textContent = event.currentTarget.value;
+
+  if (event.currentTarget.value === "") {
+    output.textContent = "Anonymus";
+  }
+}
