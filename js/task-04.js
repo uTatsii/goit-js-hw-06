a88dd49fbfa0ef98376895b5adf79f70f1f31049
uTatsii <utatsiy@gmail.com>
2022-08-22 20:24:53 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,15 @@
 let counterValue = 0;
 
-const incrementBtn = document.querySelector('[data-action=increment]');
+const counterValueSpan = document.querySelector("#value");
 const decrementBtn = document.querySelector("[data-action=decrement]");
+const incrementBtn = document.querySelector('[data-action=increment]');
 
+const decrementClick = () => {
+    counterValueSpan.textContent = counterValue += -1;
+};
+const incrementClick = () => {
+    counterValueSpan.textContent = counterValue += 1;
+}
+
+decrementBtn.addEventListener("click", decrementClick);
+incrementBtn.addEventListener('click', incrementClick)
