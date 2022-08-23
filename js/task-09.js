fa0f6a880952aace3b904ae07f87c7f3f14601f5
uTatsii <utatsiy@gmail.com>
2022-08-23 23:54:35 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.querySelector('body');
+const colorName = document.querySelector('.color');
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", onBodyChangeColor);
+
+function onBodyChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+
+  colorName.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
