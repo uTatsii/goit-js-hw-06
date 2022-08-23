b90c8faf1a62c216eae70a4c5baad65ffe05c825
uTatsii <utatsiy@gmail.com>
2022-08-23 23:58:58 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,18 @@
+
+
+
+
+
+
+
+function createBoxes(amount) {
+
+}
+
+function destroyBoxes() {
+  
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
