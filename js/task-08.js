18920f7c985a6914e01916723e3c97d63c82547a
uTatsii <utatsiy@gmail.com>
2022-08-23 23:42:30 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        email, password
+    }
+
+    if (email === "" || password === "") {
+      alert("Заполните все поля перед отправкой формы!");
+    } else {
+      console.log(formData);
+      form.reset();
+    }
+    }
