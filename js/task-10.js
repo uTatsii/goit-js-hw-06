d6ad00ffd0c1dc24fcbc3f2daba373e0bdabd57e
uTatsii <utatsiy@gmail.com>
2022-08-26 15:07:13 +0300

    fix color function

--- js/task-10.js
+++ js/task-10.js
@@ -7,7 +7,9 @@ createBtn.addEventListener("click", createBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 function createBoxes(amount) {
