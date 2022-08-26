3cbc7d4fc588cff1d609bbc98f4cf299ff4b7f7d
uTatsii <utatsiy@gmail.com>
2022-08-26 14:57:32 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const input = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
+text.style.fontSize = '56px';
+
 input.addEventListener("input", getCurrentFontSize);
 
 function getCurrentFontSize(event) {
